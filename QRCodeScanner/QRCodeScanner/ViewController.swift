82170b8fea6f6e29c16908fa07fa37383f601783
NeoTerminal <samyy.abcd@gmail.com>
2015-12-28 17:05:13 -0500

    added a label and button to access camera
    added a label and button to access camera and a green box to show a qr
    code is detected. Once a qr code is detected, the AVMetaDataMachine
    reads the url associated with that qr code and displays it in the label

--- QRCodeScanner/QRCodeScanner/ViewController.swift
+++ QRCodeScanner/QRCodeScanner/ViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
+{
+    
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
+    
+    @IBOutlet weak var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,74 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func scanMeButtonPressed(sender: AnyObject)
+    {
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input: AnyObject!
+        
+        do {
+            
+            input = try AVCaptureDeviceInput(device: captureDevice)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if (error != nil)
+        {
+            print("\(error?.localizedDescription)")
+            return
+        }
+        
+        captureSession = AVCaptureSession()
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        let captureMetaDataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetaDataOutput)
+        
+        captureMetaDataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetaDataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        
+        view.bringSubviewToFront(label)
+        
+        // bring the green code box into view
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0
+        {
+            qrCodeFrameView?.frame = CGRectZero
+            label.text = "No QR code detected"
+            return
+        }
+        
+        let metaDateObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metaDateObj.type == AVMetadataObjectTypeQRCode
+        {
+            let barcodeObject = videoPreviewLayer?.transformedMetadataObjectForMetadataObject(metaDateObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            qrCodeFrameView?.frame = barcodeObject.bounds
+            
+            if metaDateObj.stringValue != nil
+            {
+                label.text = metaDateObj.stringValue
+            }
+        }
+    }
 
 }
 
